52553f807db6794999658944bc4bb67c128aff73
mhoppitt <mhoppitt18@outlook.com>
2025-01-06 15:59:59 +1100

    Initial Commit

---
+++ FurFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FurFinder
+//
+//  Created by Matt Hoppitt on 6/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FurFinder/FurFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurFinderApp.swift
+//  FurFinder
+//
+//  Created by Matt Hoppitt on 6/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct FurFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
